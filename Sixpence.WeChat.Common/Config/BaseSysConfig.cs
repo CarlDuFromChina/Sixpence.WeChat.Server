cb44f36405a42757bb1e2af87d9c99487a45a42b
Karl Du <18556906294@163.com>
2022-08-30 15:27:15 +0800

    sixpence.web 1.3.0

--- Sixpence.WeChat.Common/Config/BaseSysConfig.cs
+++ Sixpence.WeChat.Common/Config/BaseSysConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Newtonsoft.Json;
+using Sixpence.Common.Utils;
 using Sixpence.ORM.Entity;
 using Sixpence.ORM.EntityManager;
 using Sixpence.Web.Module.SysConfig;
@@ -18,6 +19,7 @@ namespace Sixpence.WeChat
                 code = EntityCommon.UpperChartToLowerUnderLine(typeof(T).Name.Replace("Config", ""))
             };
             var result = em.DbClient.QueryFirst<sys_config>(sql, param);
+            AssertUtil.IsNull(result, $"未找到[{typeof(T).Name}]配置节点");
             Config = JsonConvert.DeserializeObject<T>(result.value);
         }
     }
